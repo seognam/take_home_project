c2e09c3c814b16fa6b98a26be631e949dbb347f5
Michael Wright <wright.mhw@gmail.com>
2019-07-06 08:22:03 +0000

    WIP of counter approach'

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# take_home_project
+# Take Home Project
+
+## Hosted online at <TBD>
+
+## Instructions
 
 Postman Echo is a service you can use to test your REST clients and make sample API calls. It provides endpoints for GET, POST, PUT, various auth mechanisms and other utility endpoints.
 The documentation for the endpoints as well as example responses can be found at https://postman-echo.com
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-import requests
+import time
 import json
 from six.moves.urllib.parse import urlparse, parse_qs
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import asyncio
+from aiohttp import web
 from aiohttp import ClientSession
 
 
@@ -11,92 +12,49 @@ INVALID_API_ERROR = {'message': 'invalid API endpoint', 'response': 'error'}
 INVALID_ITERATION_ERROR = {'message': 'invalid iterations for /count', 'response': 'error'}
 
 
-def get_iterations_from_query_string(url, counting_var):
-    path = urlparse(url)
-    query_string = parse_qs(path.query)
-    if not query_string:
-        return False
+class Server:
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
 
-    iterations = int(query_string.get(counting_var)[0])
-    if iterations < 1:
-        return False
+    async def count(self, request):
+        iterations = int(request.query['x'])
+        # return web.Response(text=json.dumps(res))
 
-    return iterations
-
-
-def lookup_route(url, valid_routes):
-    path = urlparse(url)
-    return path.path in valid_routes
-
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
-    def write_json(self, content):
-        # TODO doc
-        self.wfile.write(json.dumps(content, indent=2).encode())
-
-    def write_bytes(self, content):
-        # TODO doc
-        formated_json = json.dumps(json.loads(content.decode('utf8').replace("'", '"')),indent=2)
-        self.wfile.write(formated_json.encode())
-
-    def _set_headers(self):
-        # TODO doc
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-    def do_HEAD(self):
-        # TODO doc
-        # TODO remove?
-        self._set_headers()
-
-    def do_GET(self):
-        # TODO doc
-        # send_response and end_headers needed to make this a valid, successful response
-        self._set_headers()
-
-        is_valid_route = lookup_route(self.path, valid_routes=['/count'])
-        if not is_valid_route:
-            self.write_json(INVALID_API_ERROR)
-            return
-
-        iterations = get_iterations_from_query_string(self.path, 'x')
-        if not iterations:
-            self.write_json(INVALID_ITERATION_ERROR)
-            return
-
-        loop = asyncio.get_event_loop()
         tasks = []
 
-        async def async_get(url):
-            async with ClientSession() as session:
-                async with session.get(url) as response:
-                    r = await response.read()
-                    self.write_bytes(r)
+        # TODO add try/except e.g. https://tutorialedge.net/python/create-rest-api-python-aiohttp/#post-requests-and-query-parameters
+        async with ClientSession() as session:
+            for i in range(0, iterations):
+                get_value = i + 1
+                GET_url = BASE_GET_URL.format(get_value)
+                task = asyncio.ensure_future(self.async_get(session, GET_url))
+                tasks.append(task)
+                responses = await asyncio.gather(*tasks)
 
-        for i in range(0, iterations):
-            get_value = i + 1
-            GET_url = BASE_GET_URL.format(get_value)
-            task = asyncio.ensure_future(async_get(GET_url))
-            tasks.append(task)
+        return web.json_response(responses)
 
-        loop.run_until_complete(asyncio.wait(tasks))
+    async def async_get(self, session, url):
+        async with session.get(url) as response:
+            resp = response.text()
+            print(resp)
+            return await(resp)
+    
+    async def create_app(self):
+        app = web.Application()
+        app.router.add_get('/count', self.count)
+        return app
 
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
-    # TODO doc
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    except Exception:
-        pass
-    httpd.server_close()
+    def run(self):
+        loop = self.loop
+        app = loop.run_until_complete(self.create_app())
+        web.run_app(app)
 
 if __name__ == "__main__":
-    run()
+    server = Server()
+    server.run()
+
+# TODO asyncio.run_in_executor(json.dums)
 
 # TODO should I host this somewhere? Digital Ocean would be easiest?
 # TODO set notabs in vim
